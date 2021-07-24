bf5938344b4ac25b8199cbbb3ca46c4de4221164
hailita1 <hailit@gmail.com>
2021-07-24 16:29:43 +0700

    done

--- src/main/java/com/example/demo/service/house/HouseService.java
+++ src/main/java/com/example/demo/service/house/HouseService.java
@@ -5,11 +5,8 @@ import com.example.demo.model.auth.User;
 import com.example.demo.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 public class HouseService implements IHouseService {
